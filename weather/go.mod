0a439fefd351960d2de7937d09e1e7fef3f4cf0d
taiebhossain <taieb.hossain006@gmail.com>
2024-05-25 19:36:48 +0600

    updated

---
+++ weather/go.mod
@@ -0,0 +1,3 @@
+module github.com/taiebhossain/Golang_projects
+
+go 1.21.1
--- weather/main.go
+++ weather/main.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Weather...")
+	fmt.Println("Weather...")
+	fmt.Println("Weather...")
+	fmt.Println("Weather...")
 }
